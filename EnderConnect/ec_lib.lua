f3643b18086dfe6d82ae7bbd73af08c836070e96
NickSProud <nicksproud@gmail.com>
2026-05-17 13:20:08 +0100

    Version 2, Updating start up to collect infomation about the computer it runs on.

--- EnderConnect/ec_lib.lua
+++ EnderConnect/ec_lib.lua
@@ -1,22 +1,18 @@
-local version = 1
+local version = 2
 local github_location = "https://raw.githubusercontent.com/NickSProud/EnderConnect/main/"
 local manifest_location = "manifest.json"
 local lib = {}
+lib.version = version
 
 function lib.fetchOnlineManifest() 
     print ("Fetching Manifest")
     local raw_manifest = http.get(github_location .. manifest_location)
 
     if raw_manifest then
-        if raw_manifest.getResponseCode() == 200 then
-            local manifest = textutils.unserializeJSON(raw_manifest.readAll())
-            raw_manifest.close()
-            print("Fetched Manifest")
-            return manifest
-        else
-            print("Manifest not fetched, Responce Code: " .. raw_manifest.getResponseCode())
-            raw_manifest.close()
-        end
+        local manifest = textutils.unserializeJSON(raw_manifest.readAll())
+        raw_manifest.close()
+        print("Fetched Manifest")
+        return manifest
     else
         print("Failed to reach GitHub.")
     end
@@ -27,21 +23,19 @@ function lib.updateFile(manifest, fileName)
     print("Updating ".. fileName)
     local raw_file = http.get( github_location .. manifest[fileName].path)
     if raw_file then
-        if raw_file.getResponseCode() == 200 then
-            local file = fs.open(manifest[fileName].path..".tmp","w")
-            file.write(raw_file.readAll())
-            file.close()
-            raw_file.close()
-            if fs.exists(manifest[fileName].path) then
-                fs.delete(manifest[fileName].path)
-            end
-            fs.move(manifest[fileName].path ..".tmp", manifest[fileName].path)
-            return true
-        else
-            print("File not fetched, Responce Code: " .. raw_file.getResponseCode())
-            raw_file.close()
-            return false
+        local directory = string.match(manifest[fileName].path, "(.+)/[^/]+")
+        if directory then
+            fs.makeDir(directory)
         end
+        local file = fs.open(manifest[fileName].path..".tmp","w")
+        file.write(raw_file.readAll())
+        file.close()
+        raw_file.close()
+        if fs.exists(manifest[fileName].path) then
+           fs.delete(manifest[fileName].path)
+        end
+        fs.move(manifest[fileName].path ..".tmp", manifest[fileName].path)
+        return true
     else
         print("Failed to reach GitHub.")
     end
@@ -64,6 +58,45 @@ function lib.checkLocalVersion(filePath)
             return tonumber(versionString)
         end
     end
+    print("Could not parse version string. Defaulting to 0.")
+    return 0
+end
+
+function lib.scanForPeripherals()
+    local foundPeripherals = {}
+    print("--- Scanning for Peripherals ---")
+    for _, side in ipairs(peripheral.getNames()) do
+        foundPeripherals[side] = peripheral.getType(side)
+        print("Peripheral: " .. foundPeripherals[side])
+        print("Attached: " .. side)
+        print("---")
+    end
+    return foundPeripherals
+end
+
+function lib.loadJSONFile(filePath)
+    if not fs.exists(filePath) then
+        print(filePath .. " doesn't exist")
+        return nil
+    else
+        local file = fs.open(filePath, "r")
+        local fileContents = file.readAll()
+        file.close()
+        local jsonObject = textutils.unserializeJSON(fileContents)
+        return jsonObject
+    end
+end
+
+function lib.saveJSONFile(filePath, data)
+    local file = fs.open(filePath, "w")
+    if not file then
+        print("Error: Could not open " .. filePath .. " for writing.")
+        return false
+    end
+    local jsonData = textutils.serializeJSON(data)
+    file.writeLine(jsonData)
+    file.close()
+    return true
 end
 
 return lib
--- startup.lua
+++ startup.lua
@@ -1,17 +1,83 @@
-local version = 1
-local computer_label = os.getComputerLabel()
-local computer_id = os.getComputerID()
+local version = 2
+local myLabel = os.getComputerLabel()
+local myId = os.getComputerID()
+local ec = require("/EnderConnect/ec_lib")
 
-print("EnderConnect Version:" .. version)
+print("EnderConnect Startup Version:" .. version)
+print("EnderConnect Library Version:" .. ec.version)
 
-if computer_label ~= nil then 
-    print("Computer Label: " .. computer_label)
-else
-    print("Computer Label: nil.")
+while myLabel == nil or myLabel == "" do
+    print("Please Enter a name for this Computer.")
+    write("> ")
+    myLabel = read()
+    if myLabel ~= "" then
+        os.setComputerLabel(myLabel)
+    else
+        print("Name cannot be blank!")
+    end
 end
 
-if computer_id ~= nil then 
-    print("Computer ID: " .. computer_id)
+print("Computer: " .. myLabel .. " (ID: " .. myId .. ")")
+
+local myPeripherals = ec.scanForPeripherals()
+
+--- I need to hande if there isn't a config script, possibly run an install process where it prompts the user locally if this is the first time this program is run.
+local config = ec.loadJSONFile("EnderConnect/ec_config.json")
+
+local saveConfig = false
+
+if not config then
+    print("[!] No config file found. Creating default template...")
+    config = {
+        network_id = "EnderConnect",
+        saved_hub_id = ""
+    }
+    saveConfig = true
+end
+
+if not config.role then
+    print("[!] Configuration is missing a 'role' definition.")
+    print("Please specify a role for this device (hub, node):")
+    write("> ")
+    config.role = string.lower(read())
+    saveConfig = true
+end
+
+if not config.ownerID then
+    print("[!] Configuration is missing an 'owner' definition.")
+    print("Please specify your Minecraft Username:")
+    write("> ")
+    config.ownerID = string.lower(read())
+    saveConfig = true
+end
+
+if not config.baseID then
+    print("[!] Configuration is missing a 'Base' definition.")
+    print("Please specify your Base ID:")
+    write("> ")
+    config.baseID = string.lower(read())
+    saveConfig = true
+end
+
+if saveConfig then
+        print("Saving settings...")
+    local success = ec.saveJSONFile("EnderConnect/ec_config.json", config)
+    if success then
+        print("Configuration saved successfully!\n")
+    else
+        print("CRITICAL ERROR: Failed to write configuration to disk.")
+        return -- Stop execution if saving failed
+    end
+end
+
+print("Role: " .. config.role)
+
+if config.role == "hub" then
+    print("Launching Hub Controller...")
+    -- shell.run("EnderConnect/programs/hub.lua", textutils.serialize(myPeripherals))
+elseif config.role == "node" then
+    print("Launching Node Worker...")
+    -- shell.run("EnderConnect/programs/node.lua", textutils.serialize(myPeripherals))
 else
-    print("Computer ID Not Set")
+    print("Error: Unknown role '" .. tostring(config.role) .. "' in config.")
 end
